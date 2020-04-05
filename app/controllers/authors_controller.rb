7db718279e8cb1f3fae910c89f9cca02f79c1948
Warren Harper II <warrenharper2@att.net>
2020-04-05 19:02:43 +0000

    Automatically backed up by Learn

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -8,12 +8,12 @@ class AuthorsController < ApplicationController
 
   def create
     @author = Author.new(author_params)
-   
+
    if @author.save
-   
+
    redirect_to author_path(@author)
 
-   else  
+   else
 
      render:new
 
